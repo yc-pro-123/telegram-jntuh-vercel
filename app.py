f0ac5f57ef2e9ea417135c41fb876652b0041c28
Master of None <131793324+yc-pro-123@users.noreply.github.com>
2024-11-08 15:01:46 +0530

    Update app.py Under Maintainence

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def roll_mem(rno):
 @app.route('/',methods=["POST" ,"GET"])
 def index():
     if request.method == 'POST':
-        maintainence= False
+        maintainence= True
         
         msg = request.get_json()
         #print(msg,'\n')
